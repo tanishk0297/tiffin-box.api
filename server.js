1d1c6a967c6b4aa29bf5393a5309c5519a91f5e8
tanishk0297 <tanishk0297@gmail.com>
2023-05-15 23:35:37 +0530

    new api1

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const app = express();
 
 var corsOptions = {
-  origin: "http://localhost:3000"
+  origin: "https://tiffin-box.vercel.app"
 };
 
 app.use(cors(corsOptions));
